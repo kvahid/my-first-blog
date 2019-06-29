3950cb2832e6127620c24815f89b405d27fafefa
Vahid <vahid.karimi89@gmail.com>
2019-06-29 19:54:35 +0430

    Adding new template for post details and also css for the site

--- blog/urls.py
+++ blog/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
 from . import views
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
     path('', views.post_list, name='post_list'),
+	path('post/<int:pk>/', views.post_detail, name='post_detail'),
 ]
+urlpatterns += staticfiles_urlpatterns()
